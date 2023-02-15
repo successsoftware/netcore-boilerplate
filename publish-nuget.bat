0f008de021d489ff2bba1b138713bfc18864be42
Success Software Services <116863270+successsoftware@users.noreply.github.com>
2023-02-15 08:50:47 +0700

    Delete publish-nuget.bat

--- publish-nuget.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting publish
-dotnet nuget push "artifacts\SSS.CleanArchitecture.Net6.WebApi.1.2.0.nupkg" --api-key oy2a65arnyyq7ybsvtaadpangdz7rwnudflpawz5fpcioi --source https://api.nuget.org/v3/index.json
-pause
