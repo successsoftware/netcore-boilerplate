8bdb7e23ade47754f92fde7d25663488d8b88c21
Success Software Services <116863270+successsoftware@users.noreply.github.com>
2023-02-16 11:49:16 +0700

    Add dummy data for demostration

--- generator.bat
+++ generator.bat
@@ -3,4 +3,5 @@ echo dotnet build.
 dotnet build
 echo package template to nuget.
 dotnet pack .\templates\templates.csproj -o .\artifacts\ --no-build
+dotnet nuget push "$PWD/nuget/*.nupkg" --api-key VYHepH2s+p/+fdsfdsfsdfdsfsfdsfsdfsdfsfsdfsdfsdfsd --source "https://api.nuget.org/v3/index.json"
 pause
